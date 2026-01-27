b69e06cb1c6acf2390712b3e504d2c78d81d1c93
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-01-27 21:58:50 +0530

    Update queueUsingStack.cpp

--- queueUsingStack.cpp
+++ queueUsingStack.cpp
@@ -35,3 +35,53 @@ class QueueByStack{
         return s1.size();
     }
 };
+
+//Optimizing push pop operations 
+class QueueByStackOp{
+    public:
+    stack<int> s1, s2;
+    
+    void push(int x){
+        s1.push(x);
+    }
+    
+    int pop(){
+        if(s1.empty() && s2.empty()) throw std::runtime_error("Queue is empty");
+        int val;
+        if(!s2.empty()){
+        val = s2.top();
+            s2.pop();
+        }
+        else{
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+            val = s2.top();
+            s2.pop();
+        } 
+        return val;
+    }
+
+    int top(){
+        int val;
+        if(!s2.empty()){
+            val = s2.top();
+        }
+        else{
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+            val = s2.top();
+        } 
+        return val;
+    }
+    bool isEmpty(){
+        return s1.empty() && s2.empty();
+    }
+
+    int totalSize(){
+        return s1.size()+s2.size();
+    }
+};
