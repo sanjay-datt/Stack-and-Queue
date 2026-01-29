99aa8e22728e6c324d64c8df6780accf721b8950
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2026-01-29 20:59:48 +0530

    Create printString.cpp

---
+++ printString.cpp
@@ -0,0 +1,12 @@
+// Print String
+void printStr(string str)
+{
+    int n = str.length();
+    cout << endl
+         << "String is :";
+    for (int i = 0; i < n; i++)
+    {
+        cout << str[i];
+    }
+    cout << endl;
+}
